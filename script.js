75dc20e661200bfd02db05e2acbddb6711e33037
wqrvsf <98298812+wqrvsf@users.noreply.github.com>
2023-04-08 15:52:37 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const input = document.querySelector('#taskInput');
 const btn = document.querySelector('#addBtn');
 const ul = document.querySelector('#list');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
+    addTask();
+    input.value = '';
+});
+doneTask();
+deleteTask();
+
+function addTask() {
     const li = document.createElement('li');
     li.className = 'item';
     li.textContent = input.value;
@@ -16,18 +23,65 @@ btn.addEventListener('click', function(){
     i.className = 'fa-regular fa-square-check';
     block.append(i);
 
-    i.addEventListener('click', function(){
-        li.classList.toggle('done');
-    });
-
     const i2 = document.createElement('i');
     i2.className = 'fa-solid fa-trash-can';
     block.append(i2);
 
-    i2.addEventListener('click', function(){
+    i2.addEventListener('click', function () {
         li.parentNode.removeChild(li);
     });
-});
+}
+
+function doneTask() {
+    ul.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target.classList.contains('fa-square-check')) {
+            target.closest('li').classList.toggle('done');
+            console.log(target.closest('li'));
+            localStorage.setItem('htmlLS', ul.innerHTML);
+        }
+    })
+}
+
+
+function deleteTask() {
+    ul.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target.classList.contains('fa-trash-can')) {
+            target.closest('li').remove();
+        }
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
